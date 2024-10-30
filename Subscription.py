079be41a975e44e95862e015e71dbfab35fcf446
Monikasaikappagantula <147476363+Monikasaikappaguntula@users.noreply.github.com>
2024-10-30 10:30:12 +0530

    Uploading code: Subscription.py

---
+++ Subscription.py
@@ -0,0 +1,18 @@
+import java.util.Scanner;;
+public class Sc
+{
+   public static void main(String args[])
+   {
+    Scanner sc=new Scanner(System.in);
+    int x=sc.nextInt();
+    if(x>40)
+    {
+        System.out.println("YES");
+    }
+   
+   else
+   {
+    System.out.println("NO");
+   }
+   }
+}
